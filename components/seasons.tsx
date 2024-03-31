5b620b5613410cc3bb866d07f2022b4f21c72408
Jordan Williams <jordangabrielwilliams@gmail.com>
2024-03-31 12:00:43 -0700

    Use the Table components

--- components/seasons.tsx
+++ components/seasons.tsx
@@ -5,6 +5,15 @@ import { members } from '@/data/members';
 import type { Season } from '@/data/seasons';
 import { seasons, seasonsData } from '@/data/seasons';
 
+import {
+	Table,
+	TableHead,
+	TableHeadCell,
+	TableBody,
+	TableRow,
+	TableCell,
+} from '@/components/table';
+
 export function Seasons() {
 	const [season, setSeason] = useState<Season>(seasons[0]);
 
@@ -12,35 +21,34 @@ export function Seasons() {
 
 	return (
 		<div>
-			<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Place</th>
-						<th>Wins</th>
-						<th>Losses</th>
-						<th>Ties</th>
-					</tr>
-				</thead>
-				<tbody>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableHeadCell>Name</TableHeadCell>
+						<TableHeadCell>Place</TableHeadCell>
+						<TableHeadCell>Wins</TableHeadCell>
+						<TableHeadCell>Losses</TableHeadCell>
+						<TableHeadCell>Ties</TableHeadCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
 					{members.map(({ firstName, id }) => {
 						const {
 							place,
 							record: { wins, losses, ties },
 						} = seasonData[id];
-
 						return (
-							<tr key={id}>
-								<td>{firstName}</td>
-								<td>{place}</td>
-								<td>{wins}</td>
-								<td>{losses}</td>
-								<td>{ties}</td>
-							</tr>
+							<TableRow key={id}>
+								<TableCell>{firstName}</TableCell>
+								<TableCell>{place}</TableCell>
+								<TableCell>{wins}</TableCell>
+								<TableCell>{losses}</TableCell>
+								<TableCell>{ties}</TableCell>
+							</TableRow>
 						);
 					})}
-				</tbody>
-			</table>
+				</TableBody>
+			</Table>
 		</div>
 	);
 }
