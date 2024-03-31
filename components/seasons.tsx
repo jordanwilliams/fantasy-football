0200aad9442934a5e193a9e168013535189f3537
Jordan Williams <jordangabrielwilliams@gmail.com>
2024-03-30 21:40:29 -0700

    Map over the list of members instead of doing Object.entires on the seasonData

--- components/seasons.tsx
+++ components/seasons.tsx
@@ -1,28 +1,26 @@
 'use client';
-
 import { useState } from 'react';
 
-import type { MemberID } from '@/data/members';
-import type { Season, SeasonMemberData } from '@/data/seasons';
+import { members } from '@/data/members';
+import type { Season } from '@/data/seasons';
 import { seasons, seasonsData } from '@/data/seasons';
 
 export function Seasons() {
 	const [season, setSeason] = useState<Season>(seasons[0]);
 
 	const seasonData = seasonsData[season];
-	const data = Object.entries(seasonData) as [MemberID, SeasonMemberData][];
 
 	return (
 		<div>
-			{data.map(([memberId, seasonMemberData]) => {
+			{members.map(({ firstName, id }) => {
 				const {
 					place,
 					record: { wins, losses, ties },
-				} = seasonMemberData;
+				} = seasonData[id];
 
 				return (
-					<div key={memberId}>
-						<span>{memberId}</span>
+					<div key={id}>
+						<span>{firstName}</span>
 						<span>{place}</span>
 						<span>{wins}</span>
 						<span>{losses}</span>
