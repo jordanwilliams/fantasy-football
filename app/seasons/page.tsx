8e5a262be85dd1476feac4453cc3ddfb174fcc2d
Jordan Williams <jordangabrielwilliams@gmail.com>
2024-04-05 15:30:36 -0700

    Build out the seasons page

---
+++ app/seasons/page.tsx
@@ -0,0 +1,20 @@
+import styles from './page.module.css';
+import { Season } from '@/components/season';
+import { seasons } from '@/data/seasons';
+
+export default function Page() {
+	return (
+		<main className={styles.main}>
+			<div className={styles.seasonsList}>
+				{seasons.map((season) => {
+					return (
+						<div key={season} className={styles.season}>
+							<h2 className={styles.seasonHeading}>{season}</h2>
+							<Season season={season} />
+						</div>
+					);
+				})}
+			</div>
+		</main>
+	);
+}
---
+++ components/season.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { useState } from 'react';
+
+import { members } from '@/data/members';
+import type { Season } from '@/data/seasons';
+import { seasons, seasonsData } from '@/data/seasons';
+
+import {
+	TableContainer,
+	Table,
+	TableHead,
+	TableHeadCell,
+	TableBody,
+	TableRow,
+	TableCell,
+} from '@/components/table';
+
+type SeasonProps = {
+	season: Season;
+};
+
+export function Season({ season }: SeasonProps) {
+	const seasonData = seasonsData[season];
+
+	return (
+		<TableContainer>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableHeadCell>Name</TableHeadCell>
+						<TableHeadCell>Place</TableHeadCell>
+						<TableHeadCell>Record</TableHeadCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{members.map(({ firstName, id }) => {
+						const {
+							place,
+							record: { wins, losses, ties },
+						} = seasonData[id];
+
+						return (
+							<TableRow key={id}>
+								<TableCell>{firstName}</TableCell>
+								<TableCell>{place}</TableCell>
+								<TableCell>{`${wins}-${losses}-${ties}`}</TableCell>
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+}
