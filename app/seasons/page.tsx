6e5f7009d8868a23a2a6ad1295710e2923e03e4f
Jordan Williams <jordangabrielwilliams@gmail.com>
2024-04-06 00:41:58 -0700

    Update the page title on the seasons page

--- app/seasons/page.tsx
+++ app/seasons/page.tsx
@@ -1,6 +1,11 @@
+import type { Metadata } from 'next';
 import styles from './page.module.css';
 import { Season } from '@/components/season';
 import { seasons } from '@/data/seasons';
+
+export const metadata: Metadata = {
+	title: 'DLVT - Seasons',
+};
 
 export default function Page() {
 	return (
