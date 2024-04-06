dc1e0774100a867a7f9141b5e1b7b6b886047c44
Jordan Williams <jordangabrielwilliams@gmail.com>
2024-04-05 20:42:48 -0700

    Add a header

--- components/layout-header.tsx
+++ components/layout-header.tsx
@@ -1,3 +1,7 @@
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import localFont from 'next/font/local';
 
 import styles from './layout-header.module.css';
@@ -6,10 +10,54 @@ const hubotSans = localFont({
 	src: '../fonts/hubot-sans.woff2',
 });
 
+enum Routes {
+	Home = '/',
+	Seasons = '/seasons',
+}
+
 export default function LayoutHeader() {
+	const pathname = usePathname();
+
+	const [showNavigation, setShowNavigation] = useState(false);
+
+	function toggleNav() {
+		setShowNavigation(!showNavigation);
+	}
+
 	return (
-		<header className={`${styles.header} ${hubotSans.className}`}>
-			<h1 className={styles.title}>DLVT</h1>
+		<header className={styles.header}>
+			<button className={styles.button} onClick={toggleNav}>
+				<svg
+					width="15"
+					height="15"
+					viewBox="0 0 15 15"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z"
+						fill="currentColor"
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+					></path>
+				</svg>
+			</button>
+			{showNavigation && (
+				<>
+					<Link
+						href={Routes.Home}
+						className={`${styles.link} ${pathname === Routes.Home ? styles.linkActive : ''}`}
+					>
+						Home
+					</Link>
+					<Link
+						href={Routes.Seasons}
+						className={`${styles.link} ${pathname === Routes.Seasons ? styles.linkActive : ''}`}
+					>
+						Seasons
+					</Link>
+				</>
+			)}
 		</header>
 	);
 }
