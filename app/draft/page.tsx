d7b95c5113b163037227ed87b590665faa74ee34
Jordan Williams <jordangabrielwilliams@gmail.com>
2024-08-24 15:29:41 -0700

    Add draft position to the website

--- app/draft/page.tsx
+++ app/draft/page.tsx
@@ -1,10 +1,24 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
 import { members } from '@/data/members';
+import type { MemberID } from '@/data/members';
 import styles from './page.module.css';
 
 export const metadata: Metadata = {
 	title: 'DLVT - Draft Weekend',
+};
+
+const draftPositions: Record<MemberID, number> = {
+	'cody-fields': 1,
+	'max-houston': 2,
+	'dan-eckman': 3,
+	'chase-pritchett': 4,
+	'alex-piering': 5,
+	'collin-barrett': 6,
+	'ben-johnson': 7,
+	'jordan-williams': 8,
+	'kenny-copsey': 9,
+	'aaron-mack': 10,
 };
 
 export default function Page() {
@@ -38,12 +52,12 @@ export default function Page() {
 							<span className={styles.bold}>Draft Order:</span>{' '}
 							TBD
 							<ul className={styles.list}>
-								{members.map(({ teamName }) => (
+								{members.map(({ teamName, id }) => (
 									<li key={teamName}>
 										<span className={styles.bold}>
 											{teamName}:
 										</span>{' '}
-										TBD
+										{draftPositions[id]}
 									</li>
 								))}
 							</ul>
